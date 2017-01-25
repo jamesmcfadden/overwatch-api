3ab1412927840084237e4a646b274e84253d4799
James McFadden <james@jamesmcfadden.co.uk>
2017-01-25 18:37:16 +0000

    Update forceScheme method call

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         if ($this->app->environment('production')) {
-            $url->forceSchema('https');
+            $url->forceScheme('https');
         }
     }
 
